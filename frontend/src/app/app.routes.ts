0f1ef9e29c416b196b08e7da2a08dbff9770ee7f
zaid03 <terguyzaid@gmail.com>
2025-11-29 17:53:50 +0100

    added missing routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { EntComponent } from './ent/ent.component';
+import { EjeComponent } from './eje/eje.component';
+import { CentrogestorComponent } from './centrogestor/centrogestor.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProveedoreesComponent } from './proveedorees/proveedorees.component';
 import { NgModule } from '@angular/core';
@@ -8,6 +10,8 @@ import { NgModule } from '@angular/core';
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'ent', component: EntComponent },
+    { path: 'eje', component: EjeComponent},
+    { path: 'centro-gestor', component: CentrogestorComponent},
     { path: 'dashboard', component: DashboardComponent },
     { path: 'proveedorees', component: ProveedoreesComponent },
     { path: '', redirectTo: '/login', pathMatch: 'full' }, //route by default
